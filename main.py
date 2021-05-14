3f765cacff36ad833e2524ba0222e70ecb91694e
Sebastian Ruiz <sebastian.ruiz8@udea.edu.co>
2021-05-14 15:59:18 -0500

    Clean the file from prints

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ from myLanguage.repl import start_repl
 import uvicorn
 
 def main() -> None:
-    print('Bienvenido al analizador léxico de Sebas c:')
-    print('Escribe una oración para comenzar: ')
     uvicorn.run("server.api:app", host='0.0.0.0', port=8000, reload=True)
 
 
